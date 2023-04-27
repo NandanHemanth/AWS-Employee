9ac12e85535ef5e7d295734728ea973f51c0af6e
Nandan <nandan@pop-os.localdomain>
2023-04-27 12:18:29 +0530

    aws project

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.c4uka3ea8kul.us-east-1.rds.amazonaws.com"
+customuser = "aj"
+custompass = "a1j6a1j6"
+customdb = "employee"
+custombucket = "addaemployee"
+customregion = "us-east-1"
